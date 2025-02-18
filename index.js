aa144af3dceff1f6d2c1d315443003fee30c6911
Pawel <zelep@o2.pl>
2025-02-18 18:08:30 +0100

    added backend files

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mysql from "mysql"
+import cors from "cors"
+
+const app = express()
+
+
+
+const db = mysql.createConnection({
+    host:"sql7.freesqldatabase.com",
+    user:"sql7762232",
+    password:"FPfuQeVDs8",
+     database:"sql7762232",
+  
+
+})
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.json("hello this is the backendd!")
+})
+
+app.get("/books",(req,res)=>{
+    const q ="SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err);
+            return res.json(data);
+    })
+})
+
+app.post("/books",(req,res)=>{
+    const q = "INSERT INTO books (`title`,`desc`,`cover`,`price`) VALUES (?)"
+    // const values = ["title from backend", "desc from backend", "cover pick from backend"];
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+        req.body.price,
+    ];
+
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Book has been created successfully");
+    })
+})
+
+app.listen(8800, ()=>{
+    console.log("Connected to backendd");
+})
