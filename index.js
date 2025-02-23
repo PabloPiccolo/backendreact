0e3260178c2a570fc215f8d11999d3dc88f35c13
Pawel <zelep@o2.pl>
2025-02-23 10:03:26 +0100

    addex api_key2

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ import cors from "cors"
 const express = require('express');
 
 
-
-
 const app = express()
+//----------------------------------------------------------------------------------------------
 //dodanie apikey
 const apiKey = process.env.API_KEY; 
 
+// Middleware sprawdzające klucz API
+function checkApiKey(req, res, next) {
+    const userApiKey = req.headers['authorization']; // Pobierz klucz z nagłówka
+  
+    // Sprawdzenie, czy klucz API jest poprawny
+    if (userApiKey === `Bearer ${apiKey}`) {
+      return next(); // Jeśli klucz jest poprawny, przechodzimy do następnej funkcji
+    } else {
+      return res.status(403).json({ message: 'Forbidden: Invalid API Key' });
+    }
+  }
+  
+  // Używaj middleware do routów wymagających klucza API
+  app.use('/books', checkApiKey);
+//------------------------------------------------------------------------------------------------
 app.use((req,res,next)=>{
      res.setHeader('Content-Type', 'text/html; charset=utf-8');
      next();
